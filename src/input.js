7590fa9159d853517c277689ddede3a3458d1d4e
Inna <designbyinna@gmail.com>
2018-08-04 21:02:44 -0600

    hidden key

--- src/input.js
+++ src/input.js
@@ -4,9 +4,15 @@ import Weather from './weatherApi.js';
 import titleCase from 'title-case';
 import unsplash from 'unsplash-api';
 import './input.css';
+//require('dotenv').config()
 
-var clientId = '5f7824d340097ca7286f70ec303af16a56bc8f2d6326807bb0e7551b3d07f7b0'; //this is required to verify your application's requests
-unsplash.init(clientId);
+var API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+
+console.log(API_KEY);
+
+
+//var clientId = api_key; //this is required to verify your application's requests
+unsplash.init(API_KEY);
 
 
 class Input extends React.Component {
@@ -68,7 +74,7 @@ class Input extends React.Component {
                 <input type="text" value={this.state.value} onChange={this.handleChange} />
              </label>
              <input id="submitBtn" type="submit" value="Submit" />
-             <div class="resultWrap">
+             <div className="resultWrap">
                 <img id="image" src={this.state.image}/>
                  <Weather inputvalue={titleCase(this.state.value)}
                      date={this.state.date}
